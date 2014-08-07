513bafd27915ba89ce0bfae39596e628587137b7
Cosmin Luță <q4break@gmail.com>
2014-08-07 10:57:23 +0300

    Fixed setup.py for Python 2.x

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from distutils.core import setup
 from setuptools import find_packages
 from libBGG import __version__
 
+import sys
+
+if sys.version_info >= (3,):
+    long_description = open('README.txt', 'r', encoding='utf-8').read()
+else:
+    # python 2 doesn't have the 'encoding' keyword for open()
+    long_description = open('README.txt', 'r').read() 
+
 setup(
     name='libBGG',
     version=__version__,
@@ -10,7 +18,7 @@ setup(
     author='Geoff Lawler',
     author_email='geoff.lawler@gmail.com',
     description='A python interface to the boardgamegeek.com API and boardgame utils.',
-    long_description=open('README.txt', 'r', encoding='utf-8').read(),
+    long_description=long_description,
     url='https://github.com/philsstein/libBGG',
     install_requires=[],
     scripts=['bin/bgg_query', 'bin/top_rated']
