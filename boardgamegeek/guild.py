d7d7891bab5e3045c23070e3480fe27fcf6fd341
Cosmin Luță <q4break@gmail.com>
2014-08-18 14:12:08 +0300

    Improved guild page fetching and processing <location>..</location> info too

--- boardgamegeek/guild.py
+++ boardgamegeek/guild.py
@@ -10,10 +10,55 @@ class Guild(DictObject):
         log.info(u"manager    : {}".format(self.manager))
         log.info(u"website    : {}".format(self.website))
         log.info(u"description: {}".format(self.description))
+        log.info(u"country    : {}".format(self.country))
+        log.info(u"state      : {}".format(self.state))
+        log.info(u"city       : {}".format(self.city))
+        log.info(u"address    : {}".format(self.address))
+        log.info(u"postal code: {}".format(self.postalcode))
         if self.members:
-            log.info(u"members")
+            log.info(u"{} members".format(len(self.members)))
             for i in self.members:
                 log.info(u" - {}".format(i))
+
+    @property
+    def country(self):
+        return self._data.get("country")
+
+    @property
+    def city(self):
+        return self._data.get("city")
+
+    @property
+    def address(self):
+        """
+        :return: Both address fields concatenated
+        """
+        address = ""
+        if self._data.get("addr1"):
+            address += self._data.get("addr1")
+
+        if self._data.get("addr2"):
+            if len(address):
+                address += " "  # delimit the two address fields by a space
+            address += self._data.get("addr2")
+
+        return address if len(address) else None
+
+    @property
+    def addr1(self):
+        return self._data.get("addr1")
+
+    @property
+    def addr2(self):
+        return self._data.get("addr2")
+
+    @property
+    def postalcode(self):
+        return self._data.get("postalcode")
+
+    @property
+    def state(self):
+        return self._data.get("stateorprovince")
 
     @property
     def category(self):
