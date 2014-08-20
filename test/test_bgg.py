552a6342c3a83db0140e03b39357a3175cf69fdd
Cosmin Luță <q4break@gmail.com>
2014-08-20 12:50:31 +0300

    Updated unit test

--- test/test_bgg.py
+++ test/test_bgg.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import logging
 import os
 import tempfile
 import pytest
@@ -9,17 +10,31 @@ from boardgamegeek import BoardGameGeek, BoardGameGeekError
 
 progress_called = False
 
+# Kinda hard to test without having a "test" user
 TEST_VALID_USER = "fagentu007"
+TEST_USER_WITH_LOTS_OF_FRIENDS = "Solamar"        # user chosen randomly (..after a long search :)) ), just needed
+                                                  # someone with lots of friends :D
 TEST_INVALID_USER = "someOneThatHopefullyWontExistPlsGuysDontCreateThisUser"
-TEST_GUILD_ID = 669
+TEST_GAME_NAME = "Agricola"
+TEST_GAME_ID = 31260
 
+@pytest.fixture
+def bgg():
+    return BoardGameGeek()
+
+@pytest.fixture
+def null_logger():
+    # create logger
+    logger = logging.getLogger("null")
+    logger.setLevel(logging.ERROR)
+    return logger
 
 def progress_cb(items, total):
     global progress_called
     progress_called = True
 
 
-def test_caching():
+def test_no_caching():
 
     # test that we can disable caching
     bgg = BoardGameGeek(cache=None)
@@ -29,6 +44,8 @@ def test_caching():
     assert user is not None
     assert user.name == TEST_VALID_USER
 
+
+def test_sqlite_caching():
     # test that we can use the SQLite cache
     # generate a temporary file
     fd, name = tempfile.mkstemp(suffix=".cache")
@@ -41,7 +58,7 @@ def test_caching():
 
     with pytest.raises(BoardGameGeekError):
         # invalid value for the ttl parameter
-        bgg = BoardGameGeek(cache="sqlite://{}?ttl=blabla&fast_save=0".format(name))
+        BoardGameGeek(cache="sqlite://{}?ttl=blabla&fast_save=0".format(name))
 
     bgg = BoardGameGeek(cache="sqlite://{}?ttl=1000".format(name))
 
@@ -54,116 +71,221 @@ def test_caching():
     # clean up..
     os.unlink(name)
 
+#
+# Users testing
+#
 
-def test_user_fetch():
+def test_get_user_with_invalid_parameters(bgg):
+    # test how the module reacts to unexpected parameters
+    for invalid in [None, ""]:
+        with pytest.raises(BoardGameGeekError):
+            bgg.user(invalid)
+
+
+def test_get_invalid_user_info(bgg):
     global progress_called
 
-    #
-    # Kinda hard to test without having a "test" user
-    #
-    bgg = BoardGameGeek()
+    progress_called = False
+    user = bgg.user(TEST_INVALID_USER, progress=progress_cb)
 
-    user = bgg.user(TEST_VALID_USER, progress=progress_cb)
+    assert user is None
+    assert not progress_called
+
+
+def test_get_valid_user_info(bgg, null_logger):
+    global progress_called
+
+    progress_called = False
+    user = bgg.user(TEST_USER_WITH_LOTS_OF_FRIENDS, progress=progress_cb)
 
     assert user is not None
-    assert user.name == TEST_VALID_USER
+    assert user.name == TEST_USER_WITH_LOTS_OF_FRIENDS
     assert type(user.id) == int
     assert progress_called
 
     assert type(user.buddies) == list
     assert type(user.guilds) == list
 
-    progress_called = False
-
-    # test with some user that doesn't exist
-    with pytest.raises(BoardGameGeekError):
-        bgg.user(TEST_INVALID_USER)
-
     str(user)
     repr(user)
 
+    for buddy in user.buddies:
+        str(buddy)
+        repr(buddy)
 
-def test_collection_fetch():
-    bgg = BoardGameGeek()
+    for guild in user.guilds:
+        repr(guild)
 
-    with pytest.raises(BoardGameGeekError):
-        bgg.collection(TEST_INVALID_USER)
+    # for coverage's sake
+    user._format(null_logger)
 
-    c = bgg.collection(TEST_VALID_USER)
 
-    assert c is not None
-    assert c.owner == TEST_VALID_USER
-    assert type(len(c)) == int
-    assert type(c.items) == list
+#
+# Collections testing
+#
+def test_get_collection_with_invalid_parameters(bgg):
+    for invalid in [None, ""]:
+        with pytest.raises(BoardGameGeekError):
+            bgg.collection(invalid)
+
+
+def test_get_invalid_users_collection(bgg):
+
+    collection = bgg.collection(TEST_INVALID_USER)
+    assert collection is None
+
+
+def test_get_valid_users_collection(bgg, null_logger):
+
+    collection = bgg.collection(TEST_VALID_USER)
+
+    assert collection is not None
+    assert collection.owner == TEST_VALID_USER
+    assert type(len(collection)) == int
+    assert type(collection.items) == list
 
     # make sure we can iterate through the collection
-    for g in c:
-        pass
+    for g in collection:
+        assert type(g.id) == int
+        repr(g)
 
-    str(c)
-    repr(c)
+    str(collection)
+    repr(collection)
+
+    # for coverage's sake
+    collection._format(null_logger)
 
 
-def test_guild_fetch():
-    bgg = BoardGameGeek()
+#
+# Guild testing
+#
+def test_get_guild_with_invalid_parameters(bgg):
+    # test how the module reacts to unexpected parameters
+    for invalid in [None, [], {}]:
+        with pytest.raises(BoardGameGeekError):
+            bgg.guild(invalid)
 
-    guild = bgg.guild(TEST_GUILD_ID)
 
-    assert guild.id == TEST_GUILD_ID
-    assert guild.name == "BGG in Romana"
+def test_get_valid_guild_info(bgg, null_logger):
+    global progress_called
+
+    progress_called = False
+    # Test with a guild with a big number members so that we can cover the code that fetches the next pages
+    guild = bgg.guild(1229, progress=progress_cb)
+
+    assert progress_called
+    assert guild.id == 1229
+    assert guild.name == "Geek Tools"
+
+    assert guild.addr1 is None
+    assert guild.addr2 is None
+    assert guild.address is None
 
     for member in guild:
         pass
 
-    str(guild)
     repr(guild)
 
+    # for coverage's sake
+    guild._format(null_logger)
 
-def test_game_fetch():
-    bgg = BoardGameGeek()
 
-    # use an older game that's not so likely to change
-    game = bgg.game("Twister")
+def test_get_invalid_guild_info(bgg):
+    global progress_called
 
+    progress_called = False
+    guild = bgg.guild(0, progress=progress_cb)
+
+    assert guild is None
+    assert not progress_called
+
+
+#
+# Game testing
+#
+def test_get_unknown_game_info(bgg):
+    game = bgg.game("Whaaaat? An invalid game you say?")
+    assert game is None
+
+
+def test_get_game_with_invalid_parameters(bgg):
+    with pytest.raises(BoardGameGeekError):
+        bgg.game(name=None, game_id=None)
+
+    for invalid in [None, ""]:
+        with pytest.raises(BoardGameGeekError):
+            bgg.game(invalid)
+
+    for invalid in [None, "", "asd"]:
+        with pytest.raises(BoardGameGeekError):
+            bgg.game(None, game_id=invalid)
+
+
+def check_game(game):
     assert game is not None
-    assert game.name == "Twister"
-    assert game.id == 5894
-    assert game.year == 1966
-    assert game.mechanics == ["Player Elimination", "Roll / Spin and Move"]
-    assert game.min_players == 2
-    assert game.max_players == 4
-    assert game.thumbnail == "http://cf.geekdo-images.com/images/pic196428_t.jpg"
-    assert game.image == "http://cf.geekdo-images.com/images/pic196428.jpg"
-    assert game.playing_time == 10
-    assert game.min_age == 6
-    assert game.categories == ["Action / Dexterity", "Children's Game", "Party Game"]
-    assert game.families == ["Bratz", "Celebrities: Walt Disney", "Hello Kitty", "Promotional Board Games",
-                             "TV Series: Sesame Street", "TV Series: Tweenies", "Twister"]
-    assert game.designers == ["Chuck Foley", "Reyn Guyer", "Neil W. Rabens"]
+    assert game.name == TEST_GAME_NAME
+    assert game.id == TEST_GAME_ID
+    assert game.year == 2007
+    assert game.mechanics == ["Area Enclosure", "Card Drafting", "Hand Management", "Worker Placement"]
+    assert game.min_players == 1
+    assert game.max_players == 5
+    assert game.thumbnail == "http://cf.geekdo-images.com/images/pic259085_t.jpg"
+    assert game.image == "http://cf.geekdo-images.com/images/pic259085.jpg"
+    assert game.playing_time == 120
+    assert game.min_age == 12
+    assert game.categories == ["Economic", "Farming"]
 
-    assert game.publishers == ["Altap", "Arrow Games Ltd", "Basic Fun, Inc.", "Game Office",
-                               "Hasbro", "Jumbo", "Kidconnection", "Kids Fun Factory",
-                               "MB Juegos", "MB Spellen", "MB Spiele", "Milton Bradley", "Tri-ang"]
+    assert game.families == ["Agricola", "Animals: Cattle", "Animals: Sheep", "Harvest Series", "Solitaire Games"]
 
-    assert game.alternative_names == ["Bedrock",
-                                      "Enredos",
-                                      "The Jungle Book Twister",
-                                      "Let's Twist Again",
-                                      "Lil' Twister",
-                                      "Melktwister",
-                                      "Pretzel",
-                                      "Twist",
-                                      "Twister Kersteditie",
-                                      "Twister Pink",
-                                      "Wygibajtus",
-                                      "Твистер",
-                                      "פלונטר" ]
+    assert game.designers == ["Uwe Rosenberg"]
+
+    assert game.publishers == ["Lookout Games",
+                               "999 Games",
+                               "Brain Games",
+                               "Compaya.hu - Gamer Café Kft.",
+                               "Filosofia Édition",
+                               "Hobby Japan",
+                               "Hobby World",
+                               "HomoLudicus",
+                               "Korea Boardgames co., Ltd.",
+                               "Lacerta",
+                               "MINDOK",
+                               "Smart Ltd",
+                               "Stratelibri",
+                               "Swan Panasia Co., Ltd.",
+                               "Ystari Games",
+                               "Z-Man Games"]
+
+    assert game.alternative_names == ["Агрикола",
+                                      "アグリコラ",
+                                      "农场主",
+                                      "農家樂",
+                                      "아그리콜라"]
 
     # some not so exact assertions
-    assert game.users_rated >= 1965
+    assert game.users_rated >= 34000
     assert 0.0 <= game.rating_average <= 10.0
     assert 0.0 <= game.rating_bayes_average <= 10.0
 
+    assert type(game.rating_stddev) == float
+    assert type(game.rating_median) == float
+    assert game.rating_num_weights >= 0
+    assert type(game.rating_average_weight) == float
+
     # make sure no exception gets thrown
-    str(game)
     repr(game)
+
+
+def test_get_known_game_info(bgg, null_logger):
+    # use an older game that's not so likely to change
+    game = bgg.game(TEST_GAME_NAME)
+
+    check_game(game)
+
+    # for coverage's sake
+    game._format(null_logger)
+
+
+def test_get_known_game_info_by_id(bgg):
+    game = bgg.game(None, game_id=TEST_GAME_ID)
+    check_game(game)
