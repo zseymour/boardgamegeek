7aecffa0f5fa28e27f5b24d32448039d7068ab23
Zach Seymour <zseymou1@binghamton.edu>
2018-03-28 14:14:12 -0400

    New test prog

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
 import sys
 from boardgamegeek import BGGClient
 import logging
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger()
 
 bgg = BGGClient()
-QUERY="Xanathar's"
+QUERY="Achtung Cthulhu"
 games = bgg.search(QUERY, search_type=['rpgitem'])
-for g in games[:25]:
-    bg = bgg.game(game_id=g.id)
-    log.info(bg.name)
-    for s in bg.systems:
-        log.info(s)
-        log.info(bgg.family(name=s).description)
+bgg.game(game_id=games[0].id)._format(log)
         
